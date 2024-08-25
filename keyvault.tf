4516ea9b8b80de7224107b3e9a906f5af06a2a7c
Asad Arif <asad.arif@capgemini.com>
2024-08-25 11:13:41 +0100

    Don't add keyvault

--- keyvault.tf
+++ keyvault.tf
@@ -1,14 +1,14 @@
-resource "azurerm_key_vault" "example" {
-  name                        = "aatest1234"
-  location                    = "uksouth"
-  resource_group_name         = "aatest1"
-  enabled_for_disk_encryption = true
-  tenant_id                   = data.azurerm_client_config.current.tenant_id
-  purge_protection_enabled    = false
-  tags = {
-    ServiceCode = "CDO"
-    tag1        = "value3"
-  }
+# resource "azurerm_key_vault" "example" {
+#   name                        = "aatest1234"
+#   location                    = "uksouth"
+#   resource_group_name         = "aatest1"
+#   enabled_for_disk_encryption = true
+#   tenant_id                   = data.azurerm_client_config.current.tenant_id
+#   purge_protection_enabled    = false
+#   tags = {
+#     ServiceCode = "CDO"
+#     tag1        = "value3"
+#   }
 
-  sku_name = "standard"
-}
+#   sku_name = "standard"
+# }
