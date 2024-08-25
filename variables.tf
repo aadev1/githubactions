32bda69ea7cc2e98fdfa4463a52230b11c956d3f
Asad Arif <asad.arif@capgemini.com>
2024-08-25 15:23:16 +0100

    add vnet

--- variables.tf
+++
@@ -1,5 +0,0 @@
-variable "resource_group_name" {
-  description = "The name of the resource group in which the resources will be created."
-  type        = string
-  
-}
--- versions.tf
+++ versions.tf
@@ -1,3 +1,4 @@
+# Terraform Block
 terraform {
   required_version = "~> 1.9.4"
   required_providers {
@@ -5,41 +6,36 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "~> 4.0.1"
     }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.6.2"
+    }
   }
 
   # Terraform State Storage to Azure Storage Container
   backend "azurerm" {
     resource_group_name  = "aatest"
-    storage_account_name = "aatest1234"
+    storage_account_name = "aatest123456"
     container_name       = "tfstatefiles"
     key                  = "terraform.tfstate"
     use_oidc             = true
   }
+  # backend "azurerm" {
+  #   resource_group_name  = "aatest"
+  #   storage_account_name = "aatest1234"
+  #   container_name       = "tfstatefiles"
+  #   key                  = "terraform.tfstate"
+  #   use_oidc             = true
+  # }
 }
+
 # Provider Block
 provider "azurerm" {
   features {}
   use_oidc = true
 
-  subscription_id = "a905f68e-daad-4402-a2a6-dacfeb83534d"
+  # subscription_id = "55f3b8c6-6800-41c7-a40d-2adb5e4e1bd1"
+  subscription_id = "55f3b8c6-6800-41c7-a40d-2adb5e4e1bd1"
 }
 
-data "azurerm_client_config" "current" {}
-
-/*
-Play with Terraform CLI Version (We installed 1.0.0 version)
-  required_version = "~> 0.14.3" - Will fail
-  required_version = "~> 0.14"   - Will fail  
-  required_version = "= 0.14.4"  - Will fail
-  required_version = ">= 0.13"   - will pass
-  required_version = "= 1.0.0"   - will pass
-  required_version = "1.0.0"     - will pass 
-  required_version = ">= 1.0.0"   - will pass     
-
-
-Play with Provider Version (as on today latest version is 2.64.0)
-      version = "~> 2.0"             
-      version = ">= 2.0.0, < 2.60.0"
-      version = ">= 2.0.0, <= 2.64.0"     
-*/
 
